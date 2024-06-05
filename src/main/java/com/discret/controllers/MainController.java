75b156919a78b0e0336e1ddb74a4b820c482e9b4
AnatoliyZhidkov <Anatoliyzhudkov@yandex.ru>
2024-06-05 11:51:27 +0300

    Отображение админ кнопки только у админа

--- src/main/java/com/discret/controllers/MainController.java
+++ src/main/java/com/discret/controllers/MainController.java
@@ -1,13 +1,20 @@
 package com.discret.controllers;
 
+import com.discret.entity.Student;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.util.Collection;
 import java.util.NoSuchElementException;
 
 @Controller
@@ -15,6 +22,10 @@ public class MainController {
 
     @GetMapping("/main")
     public String main(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean hasAdminRole = authentication.getAuthorities().stream()
+                .anyMatch(r -> r.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("hasAdminRole", hasAdminRole);
         return "main";
     }
 
@@ -23,7 +34,10 @@ public class MainController {
         return "redirect:/main";
     }
 
-
+//    @PostMapping("/admin")
+//    public String adminPage(Model model){
+//ret
+//    }
 
 
 
